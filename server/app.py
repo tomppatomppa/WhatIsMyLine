2c871f4235bdd8b31b42d2c47f40d03e6b511e1a
tomi <tomiwest@hotmail.com>
2023-03-28 16:12:13 +0300

    add reader

--- server/app.py
+++ server/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 from pyMuReader import Reader
+import json
+
 app = Flask(__name__, static_folder="build/static", template_folder="build")
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/hello")
-def hello():
-    reader = Reader()
-    result = reader.read_file("default.pdf")
-    return result
+@app.route("/testfile")
+def read_testfile():
+    try:
+        reader = Reader()
+        reader.read_file("./testfiles/default.pdf")
+        result = json.loads(reader.to_json())
+        return  [result]
+    except FileNotFoundError as e:
+        return json.dumps(e)
+    
