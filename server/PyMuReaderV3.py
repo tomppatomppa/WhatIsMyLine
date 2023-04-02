6400d75d3011b6e16ab903601edd6e17b932b4d2
tomi <tomiwest@hotmail.com>
2023-04-02 17:34:41 +0300

    add make_lines

--- server/PyMuReaderV3.py
+++ server/PyMuReaderV3.py
@@ -29,6 +29,7 @@ class ReaderV3():
             'height': pages[0]['height'],
             'blocks': [block for d in pages for block in d['blocks']]
         }
+        self.page_width = merged_dict["width"]
         lines = [line for block in merged_dict["blocks"] for line in block["lines"]]
         merged_dict["blocks"] = [span for line in lines for span in line["spans"]]
         self.file = merged_dict
@@ -37,6 +38,8 @@ class ReaderV3():
     def to_json(self):
       
         with_scenes = self.make_scenes(self.file)
+        with_lines = self.make_lines(with_scenes)
+        
 
     def make_scenes(self, file):
         scenes = []
@@ -52,9 +55,9 @@ class ReaderV3():
                 scene_title = " ".join([current_line["text"], previous_line["text"]])
                 current_scene = {scene_title: []}
             else:
-                current_scene[scene_title].append(current_line["text"])
+                current_scene[scene_title].append(current_line)
             previous_line = current_line
-        print(scenes)
+        return scenes
 
     def is_scene(self, current_line, previous_line):
         if not previous_line:
@@ -64,7 +67,31 @@ class ReaderV3():
         section_pattern = r'^(?!.*\b[A-Z\dÄÅÖ]+\s\d)[A-Z\dÄÅÖ.-]+(?: [A-Z\dÄÅÖ-]+)*$'
         return re.match(r"^\d+$", previous_line["text"]) and re.match(section_pattern, current_line["text"])
     
+    def make_lines(self, file):
+        scene_details = file[0]
 
+        for scene in file[1:]:
+            for lines in scene.values():
+                for line in lines:
+                    self.is_actor(line["origin"][0], line["text"])
+                    
+               
+                
+
+    def is_actor(self, value, name):
+        name_pattern = r'^[A-Z0-9ÖÄÅ]+\s?\(?(\d+|[A-ZÖÄÅ]+|\([^)]*\))?\)?$'
+        center = self.page_width / 2
+        margin = self.page_width * 0.2 / 2
+        left_limit = center - margin
+        right_limit = center + margin
+       
+        if left_limit <= value <= right_limit and re.match(name_pattern, name):
+            print(f"{name} is flagged as valid name")
+            
+            return True
+        else:
+            #print(f"{name} is NOT a name")
+            return False
 if __name__ == '__main__':
     reader= ReaderV3()
     reader.read_file("filename")
