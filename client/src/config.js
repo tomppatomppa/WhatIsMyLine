583f6d010ceaccfa455b5ec416664d5993577e4e
tomi <tomiwest@hotmail.com>
2023-04-06 10:48:25 +0300

    setup config

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
-const BASE_URI =
-  process.env.REACT_APP_ENVIROMENT === 'development'
-    ? 'http://localhost:5000'
-    : process.env.REACT_APP_BASE_URI
+const BASE_URI = process.env.REACT_APP_BASE_URI || 'http://localhost:5000'
+// process.env.REACT_APP_ENVIROMENT === 'development'
+//   ? 'http://localhost:5000'
+//   : process.env.REACT_APP_BASE_URI
 
 export { BASE_URI }
