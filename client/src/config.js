af3909f1302a223b2b9d2be06a3590fbdcfd635e
tomi <tomiwest@hotmail.com>
2023-04-06 11:12:55 +0300

    change env variable on heroku

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
-const BASE_URI = process.env.REACT_APP_BASE_URI || 'http://localhost:5000'
+const BASE_URI = process.env.HEROKU_URI || 'http://localhost:5000'
 // process.env.REACT_APP_ENVIROMENT === 'development'
 //   ? 'http://localhost:5000'
 //   : process.env.REACT_APP_BASE_URI
-console.log('Base URI:', BASE_URI)
+console.log('Base URI:', BASE_URI, process.env.HEROKU_URI)
 export { BASE_URI }
