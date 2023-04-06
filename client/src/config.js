728e15f4eb83319517a7c1bc677c2def3e65d9f6
tomi <tomiwest@hotmail.com>
2023-04-06 10:56:48 +0300

    debug problem with heroku env not being set correctly

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@ const BASE_URI = process.env.REACT_APP_BASE_URI || 'http://localhost:5000'
 // process.env.REACT_APP_ENVIROMENT === 'development'
 //   ? 'http://localhost:5000'
 //   : process.env.REACT_APP_BASE_URI
-
+console.log('Base URI:', BASE_URI)
 export { BASE_URI }
