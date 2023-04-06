3c47cf020af02f9323a1f32d703e35c7da6bbb10
tomi <tomiwest@hotmail.com>
2023-04-06 11:18:55 +0300

    set base url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
-const BASE_URI = process.env.HEROKU_URI || 'http://localhost:5000'
-// process.env.REACT_APP_ENVIROMENT === 'development'
-//   ? 'http://localhost:5000'
-//   : process.env.REACT_APP_BASE_URI
-console.log('Base URI:', BASE_URI, process.env.HEROKU_URI)
+const BASE_URI =
+  process.env.REACT_APP_ENVIROMENT === 'development'
+    ? 'http://localhost:5000'
+    : 'https://dramatify.herokuapp.com'
+
 export { BASE_URI }
