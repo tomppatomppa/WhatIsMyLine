ffc02fc3c34dc906b390cb507cf7e3266d072677
tomi <tomiwest@hotmail.com>
2023-05-31 10:51:10 +0300

    create utils.ts

--- client/src/components/ReaderV3/reducer.ts
+++ client/src/components/ReaderV3/reducer.ts
@@ -1,33 +1,6 @@
 //@ts-nocheck
-import { Actor, OptionState, ReaderMenuActions } from './reader.types'
-
-function generateUniqueColor(highlight: Actor[]) {
-  const colors = [
-    '#86efac',
-    '#d8b4fe',
-    '#f7d8a1',
-    '#e6d5c1',
-    '#d6d2e0',
-    '#c6cfeb',
-    '#b5ccf8',
-    '#a5c9f3',
-    '#94c5ed',
-    '#84c2e8',
-    '#74bedf',
-    '#64bbda',
-  ]
-
-  const excludedColors = highlight.map((item) => {
-    return item.style.backgroundColor
-  })
-
-  for (const element of colors) {
-    if (!excludedColors.includes(element)) {
-      return element
-    }
-  }
-  return '#86efac'
-}
+import { OptionState, ReaderMenuActions } from './reader.types'
+import { generateUniqueColor } from './utils'
 
 export const reducer = (
   state: OptionState,
