6268bf4daa4af0ea82975692d6fc19182a509bd8
tomi <tomiwest@hotmail.com>
2023-06-01 13:25:20 +0300

    EditableSceneItem add lines

--- client/src/components/ReaderV3/components/EditableSceneItem.tsx
+++ client/src/components/ReaderV3/components/EditableSceneItem.tsx
@@ -7,6 +7,8 @@ import { useEffect, useState } from 'react'
 import ReaderMenuButton from './ReaderMenuButton'
 import ConfirmIcon from './icons/ConfirmIcon'
 import CancelIcon from './icons/CancelIcon'
+import DeleteIcon from './icons/DeleteIcon'
+import PlusIcon from './icons/PlusIcon'
 interface EditableSceneItemProps {
   scene: Scene
   isEditing: boolean
@@ -21,7 +23,9 @@ const EditableSceneItem = (props: EditableSceneItemProps) => {
   const { scene, isEditing, options, handleSave } = props
   const [formKey, setFormKey] = useState(0)
 
-  //Force form to update
+  /**
+   * Force form to rerender by updating key
+   */
   useEffect(() => {
     setFormKey((prev) => prev + 1)
   }, [isEditing])
@@ -36,22 +40,56 @@ const EditableSceneItem = (props: EditableSceneItemProps) => {
                 <EditMenu {...props} resetForm={resetForm} />
                 <fieldset disabled={!isEditing}>
                   {values.data.map(({ name, type, lines }, index) => (
-                    <div key={index} className="my-2 flex flex-col">
-                      <Field
-                        onClick={() => props.handleHighlight(name)}
-                        style={getLineStyle(type, options)}
-                        className={clsx(`font-bold cursor-pointer`)}
-                        name={`data[${index}].name`}
-                        value={name}
-                      />
-                      <FormikTextArea
-                        style={getLineStyle(type, options, name)}
-                        type={type}
-                        label="lines"
-                        id="text-area"
-                        name={`data[${index}].lines`}
-                        value={lines}
-                      />
+                    <div key={index}>
+                      {isEditing && (
+                        <label
+                          onClick={() =>
+                            insert(index, {
+                              type: 'ACTOR',
+                              name: 'Add name',
+                              lines: 'New Line',
+                            })
+                          }
+                          className="focus:bg-red-200 cursor-crosshair hover:animate-pulse flex mx-auto w-6 justify-center items-center "
+                        >
+                          <PlusIcon />
+                        </label>
+                      )}
+                      <div
+                        className={clsx(
+                          `my-2 flex flex-col ${
+                            isEditing && 'border-orange-400 border'
+                          } relative`
+                        )}
+                      >
+                        <Field
+                          onClick={() => {
+                            if (!isEditing) props.handleHighlight(name)
+                          }}
+                          style={getLineStyle(type, options)}
+                          className={clsx(`font-bold cursor-pointer`)}
+                          name={`data[${index}].name`}
+                          value={name || ''}
+                        />
+                        <FormikTextArea
+                          style={getLineStyle(type, options, name)}
+                          type={type}
+                          label="lines"
+                          id="text-area"
+                          name={`data[${index}].lines`}
+                          value={lines}
+                        />
+                        {isEditing && (
+                          <>
+                            <label
+                              onClick={() => remove(index)}
+                              className="absolute  w-6 right-0 top-0"
+                            >
+                              <DeleteIcon />
+                            </label>
+                          </>
+                        )}
+                      </div>
                     </div>
                   ))}
                 </fieldset>
