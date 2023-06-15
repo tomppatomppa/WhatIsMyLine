c103fa2f7a86123a4ec26ab07e8384cd79d69398
tomi <tomiwest@hotmail.com>
2023-06-15 12:02:59 +0300

    google drive picker auto fetch file on select

--- client/src/API/googleApi.ts
+++ client/src/API/googleApi.ts
@@ -1,11 +1,21 @@
 import axios from 'axios'
 
-export const getFileGoogleDrive = async (
-  fileId: string,
+type Docs = {
+  id: string
+  name: string
+  mimeType: string
+}
+
+interface getFileGoogleDriveProps {
+  docs: Docs
   access_token: string
-) => {
+}
+export const getFileGoogleDrive = async ({
+  docs,
+  access_token,
+}: getFileGoogleDriveProps) => {
   const { data } = await axios.get(
-    `https://www.googleapis.com/drive/v3/files/${fileId}`,
+    `https://www.googleapis.com/drive/v3/files/${docs.id}`,
     {
       headers: {
         Authorization: `Bearer ${access_token}`,
--- client/src/components/FileLoader/GooglePicker.tsx
+++ client/src/components/FileLoader/GooglePicker.tsx
@@ -2,24 +2,30 @@ import useDrivePicker from 'react-google-drive-picker'
 import { useEffect } from 'react'
 import { getFileGoogleDrive } from 'src/API/googleApi'
 import { FaGoogleDrive } from 'react-icons/fa'
-
+import { useMutation } from 'react-query'
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
 
 interface GooglePickerProps {
   className?: string
-  setAccessToken?: (access_token: string) => void
-  onFileSelect?: (result: File) => void
+  onFileSelect: (result: File) => void
   access_token?: string
 }
 
 const GooglePicker = ({
   className,
-  setAccessToken,
   access_token,
   onFileSelect,
 }: GooglePickerProps) => {
   const [openPicker, authResponse] = useDrivePicker()
+  const { mutate } = useMutation(getFileGoogleDrive, {
+    onSuccess: (pdfFile, variables) => {
+      const file = new File([pdfFile], variables.docs.name, {
+        type: variables.docs.mimeType,
+      })
+      onFileSelect(file)
+    },
+  })
 
   const token = access_token || authResponse?.access_token || ''
 
@@ -34,30 +40,13 @@ const GooglePicker = ({
       supportDrives: true,
       multiselect: false,
 
-      callbackFunction: async (data) => {
-        if (data.action === 'cancel') {
-          console.log('User clicked cancel/close button')
-        } else if (data.docs && onFileSelect && token) {
-          try {
-            const result = await getFileGoogleDrive(data?.docs[0].id, token)
-            const file = new File([result], data.docs[0].name, {
-              type: data.docs[0].mimeType,
-            })
-            console.log(result)
-            onFileSelect(file)
-          } catch (e) {
-            console.log(e)
-          }
+      callbackFunction: async (data: any) => {
+        if (data.action === 'picked') {
+          mutate({ docs: data.docs[0], access_token: token })
         }
       },
     })
   }
-
-  useEffect(() => {
-    if (authResponse?.access_token && setAccessToken) {
-      setAccessToken(authResponse.access_token)
-    }
-  }, [authResponse, setAccessToken])
 
   return (
     <button className={className} onClick={() => handleOpenPicker()}>
