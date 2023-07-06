baad23f5213225267768c3b44bd801d012d24ec2
tomi <tomiwest@hotmail.com>
2023-07-06 08:41:53 +0300

    install cypress

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
 import ReaderPage from '../containers/ReaderPage'
-import LandingPage from './PublicLayout'
 import MainLayout from './MainLayout'
 import { ProtectedRoute } from './ProtectedRoute'
 import PublicLayout from './PublicLayout'
