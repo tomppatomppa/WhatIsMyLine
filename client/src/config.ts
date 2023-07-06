86075029f3cdafa9c4b587c2ff84ea6c50abdfbf
tomi <tomiwest@hotmail.com>
2023-07-06 21:20:05 +0300

    fix invalid BASE_URI

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,7 +3,7 @@ const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
 
 const BASE_URI =
   process.env.REACT_APP_ENVIROMENT === 'development'
-    ? '/'
+    ? 'http://127.0.0.1:5000'
     : 'https://dramatify.herokuapp.com'
 
 export { CLIENT_ID, API_KEY, BASE_URI }
