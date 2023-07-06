2ebc687d520119f05d7a95fb9cf12562b1ed69c7
tomi <tomiwest@hotmail.com>
2023-07-06 19:04:43 +0300

    update pipeline

--- client/src/index.js
+++ client/src/index.js
@@ -26,8 +26,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID
 
-console.log(GOOGLE_CLIENT_ID)
-
 root.render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <QueryClientProvider client={queryClient}>
