9e36e927ddf45541afa305f947191c367fd278dd
tomi <tomiwest@hotmail.com>
2023-08-17 12:08:25 +0300

    clean up unused code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,8 @@
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './router'
-import { getUser } from './API/loginApi'
-import { useQuery } from 'react-query'
 
 function App() {
-  //useQuery(['user'], () => getUser()) // Refresh jwt by calling getUser() every time the app is loaded
-
   return (
     <div className="app text-center">
       <RouterProvider router={router} />
