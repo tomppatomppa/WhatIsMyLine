bbb6de89bc8a339cfd1f8abe5bc252072713fb96
Tomi West <68051921+tomppatomppa@users.noreply.github.com>
2025-02-22 09:51:37 +0200

    set JWT_SESSION_COOKIE

--- server/project/__init__.py
+++ server/project/__init__.py
@@ -26,6 +26,7 @@ def create_app():
     app.config["JWT_COOKIE_SECURE"] = True
     app.config["JWT_COOKIE_CSRF_PROTECT"] = True
     app.config["SESSION_COOKIE_DOMAIN"] = True
+    app.config["JWT_SESSION_COOKIE"] = False 
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=7)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
     app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
